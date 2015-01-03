cd5d269b82aeccd6a89ab33b8a95005935e0cb98
Susana C Delgadillo <susana.delgadillo@refinery29.com>
2015-01-03 17:17:48 -0500

    can read query param values

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var APP_PORT = 8001; //the port in which the application will run
-var IO_PORT = 8000; //the port in which socket io will run
-
+// Modules
 var express = require('express'); //include the express js framework
 var http = require('http');
 var socketio = require('socket.io');
 var redis = require('redis');
+var path = require('path');
 
-// Middleware
+// Middleware - have access to request and response objects
 var bodyParser = require('body-parser');
+
+// Constants
+var APP_PORT = 8001; //the port in which the application will run
+var IO_PORT = 8000; //the port in which socket io will run
+var STATIC_ROOT = path.join(__dirname, 'public');
+var VIEWS_ROOT = path.join(__dirname, 'views');
 
 // Initialize
 var app = express(); // create an app using express js
@@ -15,16 +20,14 @@ var server = http.createServer(app); //create an express js server
 var io = socketio.listen(server); //start socket io
 var redisClient = redis.createClient(); //create a redis client
 
-
-
-
 app.use(bodyParser.urlencoded({
   extended: true
 })); // use body-parser middleware to parse request body
 app.use(express.static('public')); // declare 'public' dir as the dir that will serve up static files
 
 app.get('/', function(req, res){
-  res.sendFile('./views/index.html');
+  res.sendFile('index.html', {root : VIEWS_ROOT});
+  console.log(req.param('stuff'));
 });
 
 
